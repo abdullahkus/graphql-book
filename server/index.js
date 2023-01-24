3e9c56178ebf848680ce71ba83dcbff85e4db4f4
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-01-24 04:33:48 +0300

    server host remove

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ import jsonGraphqlExpress from 'json-graphql-server';
 import cors from 'cors';
 import data from './db.js'
 
-const HOST = 'localhost';
-const PORT = 4000;
+const port = process.env.PORT || 3200; // <== You can change the port
 const app = express();
 
 app.use(cors());
 app.use('/graphql', jsonGraphqlExpress.default(data));
-app.listen(PORT, HOST);
+app.listen(port);
